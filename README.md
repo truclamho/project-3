5654c58b06e40c13d6485715615d2f21c94f0a3f
Truc Lam Ho <tho12@lsu.edu>
2025-04-05 11:13:33 -0500

    Updated Code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # project-3
+
+I decided to do the inside of a train station because it tends to have a lot of
+digital adds displayed about various subject. I thought that would be a good
+way to expereiment with text animation. For each sign in my code, I tried out
+different animations to learn. Though it was experimental, I did make sure to
+have the animation correspond to the typopgrahy. I know the project required 
+a responsive design; however, I found it was best to keep the layout landscape
+to display the train, plus, I will be honest, I found it more difficult to resize
+compared to doing it for a regular website (last project). There are quiet a few
+interactive aspects to the site such as with many of the signs and with the
+background.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-// JS scripts placed here
+const cities = ["Tokyo", "Kyoto", "Nara", "Osaka"];
+let cityContainer = document.querySelector('.city-names');
+let sign1 = document.querySelector('.sign1');
+
+let spinning = false;
+
+function spinAndStop() {
+  if (spinning) return; 
+  spinning = true; 
+
+  let citiesMarkup = cities.concat(cities).map(city => `<div>${city}</div>`).join('');
+  cityContainer.innerHTML = citiesMarkup;
+
+  cityContainer.style.animation = `spinCity 2s linear 3`;  
+
+  setTimeout(() => {
+    cityContainer.style.animation = "none";
+
+    let randomCityIndex = Math.floor(Math.random() * cities.length);
+
+    cityContainer.innerHTML = `<div>${cities[randomCityIndex]}</div>`;
+
+    spinning = false;
+  }, 2000); 
+}
+
+sign1.addEventListener('click', spinAndStop);
+
+let clickCount = 0; 
+
+window.addEventListener('click', function() {
+  clickCount++;
+
+  let lightBlue = `rgb(173, 216, 230)`; 
+  let yellow = `rgb(255, 255, 0)`;     
+  let orange = `rgb(244, 105, 6)`;       
+  let purple = `rgb(41, 5, 74)`;       
+
+  if (clickCount % 4 === 1) {
+    document.body.style.background = `linear-gradient(141deg, ${lightBlue} 0%, ${yellow} 50%, ${orange} 100%)`;
+  } else if (clickCount % 4 === 2) {
+    document.body.style.background = `linear-gradient(141deg, ${yellow} 0%, ${orange} 50%, ${purple} 100%)`;
+  } else if (clickCount % 4 === 3) {
+    document.body.style.background = `linear-gradient(141deg, ${orange} 0%, ${purple} 50%, ${lightBlue} 100%)`;
+  } else {
+    document.body.style.background = `linear-gradient(141deg, ${purple} 0%, ${lightBlue} 50%, ${yellow} 100%)`;
+  }
+});
